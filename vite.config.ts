bc8ab775b7425ef5ea26e4e212917dba6fd8b72b
p-keminer <p-keminer@users.noreply.github.com>
2026-03-17 06:10:37 +0100

    fix(build): use array input for rollup to fix 404.html not building on CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/',
+  build: {
+    rollupOptions: {
+      input: [
+        resolve(__dirname, 'index.html'),
+        resolve(__dirname, '404.html'),
+      ],
+    },
+  },
 });
